f12ebd5fed58029e632b0514b64ae26f416d8bea
Dmitry Pripa <i@dpripa.com>
2021-10-19 16:35:51 +0300

    minor fix

--- index.php
+++ index.php
@@ -12,5 +12,9 @@ namespace WP_Titan;
 
 defined( 'ABSPATH' ) || exit;
 
+if ( class_exists( 'WP_Titan\App' ) ) {
+	return;
+}
+
 require_once dirname( __FILE__ ) . '/vendor/autoload.php';
 require_once dirname( __FILE__ ) . '/functions.php';
