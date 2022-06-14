5ea61a6737efc32d24f3ccebcd1568af594c5dbe
Dmitry Pripa <i@dpripa.com>
2022-06-15 00:26:17 +0300

    clear repo

--- sources/scripts/setting.js
+++
@@ -1,9 +0,0 @@
-import initNotices from './setting/notices';
-import initPage from './setting/page';
-import initControlSelect from './setting/control/select';
-
-$( document ).on( 'ready', function() {
-	initNotices();
-	initPage();
-	initControlSelect();
-});
--- sources/scripts/setting/control/select.js
+++
@@ -1,9 +0,0 @@
-import 'select2';
-
-export default function initControlSelect() {
-	const $multiple = $( '.wpt-control-select select[multiple]' );
-
-	if ( $multiple.length ) {
-		$multiple.select2();
-	}
-}
