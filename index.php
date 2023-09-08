7b2d397697ee2cc4c940e354ad7f8b4a87323d5c
Dmitry Pripa <i@dpripa.com>
2023-09-08 21:16:19 +0300

    ...

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace Wpappy_1_0_7;
+namespace O0W7_1;
 
 defined( 'ABSPATH' ) || exit;
 
-if ( ! class_exists( 'Wpappy_1_0_7\App' ) ) {
-	define( 'Wpappy_1_0_7\ROOT_FILE', __FILE__ );
+if ( ! class_exists( 'O0W7_1\App' ) ) {
+	define( 'O0W7_1\ROOT_FILE', __FILE__ );
 
 	require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 }
